f836aafe242c104212f3c5d02c8836b758d81227
Samuel Ballan <sgb4622@gmail.com>
2019-03-10 14:30:40 -0400

    menu configs

--- src/views/singles/Menu.vue
+++ src/views/singles/Menu.vue
@@ -1,26 +1,50 @@
 <template>
-  <single-viewer class="tile">
-    <vb-menu :items="items"></vb-menu>
-  </single-viewer>
+  <div class="columns">
+    <div class="column">
+      <p>Menu Items</p>
+      <textarea v-model="modelData" cols="50" rows="10"/>
+    </div>
+      <div class="column">
+      <vb-menu :items="propData"></vb-menu>
+    </div>
+  </div>
 </template>
 
 <script>
 
 import HasPropClasses from '@/mixins/hasPropClasses'
-import SingleViewer from '@/components/layout/SingleViewer'
 
 import VbMenu from '@/components/VbMenu'
 
 export default {
   name: 'Menu',
   mixins: [ HasPropClasses ],
-  components: { SingleViewer, VbMenu },
+  components: { VbMenu },
+  computed: {
+    labels() { return this.labelNames.split(",")},
+    propData() {
+      try {
+        return JSON.parse(this.modelData)
+      }catch(err) {
+        return {}
+      }
+    }
+  },
   data() {
     return {
+      modelData: `{
+        "General": ["Admministration", "Team Settings"],
+        "Administration": ["Invitations", "Authentication"],
+        "Transactions": ["Payments", "Transfers", "Balance"]
+      }`,
       items: {
         General: ['Admministration', 'Team Settings'],
         Administration: ['Invitations', 'Authentication'],
         Transactions: ['Payments', 'Transfers', 'Balance']
+      },
+      labelNames: "General, Addmition",
+      childrenNames: {
+        General: ['SubItems', 'Others']
       }
     }
   }
